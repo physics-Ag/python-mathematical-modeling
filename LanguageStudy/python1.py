94d7ba07017f6e73adb312855027b9d49f80716b
physics_Ag <16252845+physics_Ag@user.noreply.gitee.com>
2025-12-19 23:14:24 +0800

    LanguageStudy

---
+++ LanguageStudy/python1.py
@@ -0,0 +1,17 @@
+# 1、python是一个动态类型的语言，type(变量名)会返回类型字符串
+a = 10
+print(type(a))
+
+a = 0.5
+print(type(a))
+
+a = "hello world"
+print(type(a))
+
+# True首字母要大写
+a = True
+print(type(a))
+
+
+# 2、python的除法，整数 / 整数不会截断小数
+print(2 / 3)
