71703a014dc1393f9bdfacbb786967623a3c94c4
physics_Ag <16252845+physics_Ag@user.noreply.gitee.com>
2026-01-04 00:18:45 +0800

    NumPy

--- NumPyStudy/NumPy4.py
+++ NumPyStudy/NumPy4.py
@@ -9,16 +9,14 @@ def show(a, b, c):
 A = np.array([[1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6]])
 
 a = np.sum(A)
-b = np.sum(A, axis = 0)
-c = np.sum(A, axis = 1)
+b = np.sum(A, axis = 0)     # 表示对列求和
+c = np.sum(A, axis = 1)     # 表示对行求和
 show(a, b, c)
 
 a = np.prod(A)
-b = np.prod(A, axis = 0)
-c = np.prod(A, axis = 1)
+b = np.prod(A, axis = 0)    # 表示对列求积
+c = np.prod(A, axis = 1)    # 表示对行求积
 show(a, b, c)
 
-a = np.power(A)
-b = np.power(A, axis = 0)
-c = np.power(A, axis = 1)
-show(a, b, c)
+a = np.power(A, 2)
+print(a)
