b5ef049c178803327e243985110af31a59853b3e
physics_Ag <16252845+physics_Ag@user.noreply.gitee.com>
2026-01-03 23:55:59 +0800

    NumPy4

--- NumPyStudy/NumPy4.py
+++ NumPyStudy/NumPy4.py
@@ -1,4 +1,24 @@
 import numpy as np
 
+def show(a, b, c):
+    print(a)
+    print(b)
+    print(c)
+
 # 7、np.sum函数、np.prod函数、np.power函数
-# 8、数组的广播
+A = np.array([[1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6]])
+
+a = np.sum(A)
+b = np.sum(A, axis = 0)
+c = np.sum(A, axis = 1)
+show(a, b, c)
+
+a = np.prod(A)
+b = np.prod(A, axis = 0)
+c = np.prod(A, axis = 1)
+show(a, b, c)
+
+a = np.power(A)
+b = np.power(A, axis = 0)
+c = np.power(A, axis = 1)
+show(a, b, c)
