a33dd1787e66816f669975d4bb638411cb334c26
physics_Ag <16252845+physics_Ag@user.noreply.gitee.com>
2026-05-10 16:00:12 +0800

    协程_yield

---
+++ LanguageStudy/python29.py
@@ -0,0 +1,72 @@
+from typing import Iterator
+import asyncio
+
+# 60、yield关键字
+# 1、yield搭配循环 -- 在循环里面的yield不会退出, 而是把每次返回的内容保存在一个可迭代对象里面
+# split_into_string的作用是把字符串按句号进行 分割 并返回一个 分割好的字符串迭代对象
+def split_into_string(input: Iterator[str]) -> Iterator[str]:
+    tmp = ""
+    for i in input:
+        tmp += i
+        while "。" in tmp:
+            stop_index = tmp.index("。")
+            yield tmp[:stop_index]
+            tmp = tmp[stop_index + 1:]
+    yield tmp
+# 2、yield不搭配循环单独使用, 会直接返回一个函数对象, 这个函数对象记录当前执行到的位置, 通过next函数跳转
+def func():
+    yield "第一次返回"
+    print("准备ing")
+    yield "第二次返回"
+    print("准备ing")
+    yield "第三次返回"
+a = split_into_string("1。我的娃。无法测温。范围。123123")
+for i in a:
+    print(i)
+# 不同函数对象
+print(next(func()))
+print(next(func()))
+print(next(func()))
+# 同一函数对象
+a = func()
+print(next(a))
+print(next(a))
+print(next(a))
+
+# 61、python协程
+# 协程的本质：在一个事件循环中，如果出现了等待任务，可以在等待过程中执行其它任务(单进程/线程)
+# 协程的原理：1、协程需要一个事件循环even loop, 该事件循环会在 任务 交出执行权时选择当前的最佳任务
+#            2、对于async def/with/for xxxx的部分, 调用时会直接返回一个coroutine对象, 注意!它不是一个任务
+#            3、asyncio.run(xxx())会创建事件循环, 并将xxx()返回的coroutine对象转化为一个任务
+#            4、await coroutine对象, 首先会把coroutine对象变成任务, 其次会声明当前任务必须在该任务执行完后才能执行，最后会把执行权交给事件循环
+#            5、task1 = asyncio.create_task(func1())可以把func1()返回的coroutine对象变成任务
+async def func1():
+    # 创建任务, 同时交出执行权
+    await asyncio.sleep(2)
+    print("func1()")
+
+async def func2():
+    # 创建任务, 同时交出执行权
+    await asyncio.sleep(5)
+    print("func2()")
+
+async def main():
+    # 创建任务
+    task1 = asyncio.create_task(func1())
+    task2 = asyncio.create_task(func2())
+
+    # 创建任务, 同时交出执行权
+    await asyncio.sleep(3)
+    print("main()")
+
+    # 只是声明任务依赖关系
+    await task1
+    await task2
+
+# 创建事件循环 + 任务main
+asyncio.run(main())
+
+# 62、tying包
+# 本质就是对Python类型进行扩展的包
+# Iterator[str]表示返回一个可迭代对象, 迭代的内容是str
+# Iterator[List[str]]表示返回一个可迭代对象, 迭代的内容是List[str]
