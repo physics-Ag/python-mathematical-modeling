4545cafa5da9044f106c5a57935d48f0ddc9c3a5
physics_Ag <16252845+physics_Ag@user.noreply.gitee.com>
2026-07-04 18:28:53 +0800

    set类型

--- LanguageStudy/python31.py
+++ LanguageStudy/python31.py
@@ -1,7 +1,7 @@
 from typing import Optional
 from pydantic import Field
 
-# python说明类成员的属性的方法
+# 65、python说明类成员的属性的方法
 # 注: (Optional[str]) 等价于 (str | none)
 class test:
     # 下面的self.name无法说明, 只能强制定义
@@ -9,3 +9,14 @@ class test:
     name: Optional[str] = Field(default=None, description="这个人的名字")
     def __init__(self):
         self.name = "123"
+
+# 66、set数据类型(集合类型)
+# set类型的定义方法:
+# (1)、使用set()函数
+print(type(set([1, 1, 2, 2, 3, 3])))
+print(type(set({1, 1, 2, 2, 3, 3})))
+# (2)、使用{} -- 注意: { "name" : "张三"}是字典类型 ; { 1, 2, 3 }是集合类型
+print(type({1, 2, 2, 3, 4}))
+# 去重特性
+print(set([1, 1, 2, 2, 3, 3]))
+print(set({1, 1, 2, 2, 3, 3}))
